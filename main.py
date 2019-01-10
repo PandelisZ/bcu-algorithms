0ab03e1155cdbf38e6e8d83890ea76fbb688c0ac
Pandelis Zembashis <info@pandelis.me>
2019-01-10 21:23:08 +0000

    Output readability

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ Created on Tue Nov 27 15:57:10 2018
 """
 VERBOSE = False
 import warnings
+import time
+import pandas as pd
+import datetime
 warnings.filterwarnings('ignore')
 
-def printScore(score_array, label,modelName):
+def printScore(score_array, label):
     import numpy as np
     avg = np.mean(score_array)
     std = np.std(score_array)
-    print("{:s}. {:s}. Mean: {:f} - Standard Deviation: {:f}".format(modelName,label,avg,std))
+    print("    {:s} - Mean: {:f} - Standard Deviation: {:f}".format(label,avg,std))
 
 #Evaluates model and gives f1-score. Generic to all algorithms
 def scoreModel(model, X_test, y_test):
@@ -25,6 +28,19 @@ def scoreModel(model, X_test, y_test):
         print(classification_report(y_test,y_pred))
     return [f1_score(y_test, y_pred,average='weighted'), precision_score(y_test, y_pred,average='weighted'), recall_score(y_test, y_pred,average='weighted')]
 
+def model_timer(func):
+    def model_timer_wraper(*args, **kwargs):
+        time1 = time.time()
+        func(*args, **kwargs)
+        time2 = time.time()
+        runtime = (time2 - time1)
+        m, s = divmod(runtime, 60)
+        h, m = divmod(m, 60)
+        print("    Algorithm took: %d:%02d:%02d (H:MM:SS)" % (h, m, s))
+    return model_timer_wraper
+
+
+@model_timer
 def foldData(modelName, X,y):
     from sklearn.model_selection import StratifiedKFold
     kfold = StratifiedKFold(10, True, 1)
@@ -37,9 +53,11 @@ def foldData(modelName, X,y):
         f1_score_array.append(scores[0])
         precision_score_array.append(scores[1])
         recall_score_array.append(scores[2])
-    printScore(f1_score_array, "F1 Score",modelName)
-    printScore(precision_score_array, "Precision Score",modelName)
-    printScore(recall_score_array, "Recall Score",modelName)
+
+    print("Algorithm: %s" % modelName)
+    printScore(f1_score_array, "F1 Score")
+    printScore(precision_score_array, "Precision Score")
+    printScore(recall_score_array, "Recall Score")
 
 #Method for training a specific model. Returns the model MODIFY FOR EACH ALGORITHM
 def trainModel(modelName, X_train, y_train):
@@ -68,17 +86,8 @@ def trainModel(modelName, X_train, y_train):
         model = KNeighborsClassifier(n_neighbors=3)
         return model.fit(X_train, y_train)
 
-def timeFunction(modelName):
-    import time
-    time1 = time.time()
-    ofsted()
-    time2 = time.time()
-    runtime = (time2 - time1) / 60
-    print(modelName, "algorithm " "took" , runtime, "minutes")
-
 #Main method tying together the whole flow and defines file/column names
-def ofsted():
-    import pandas as pd
+def main():
 
     # Read in traning data
     data_x = pd.read_csv('data/x.csv')
@@ -88,16 +97,16 @@ def ofsted():
     data_x_clean = data_x.drop("Unnamed: 0", axis=1)
     data_y_clean = data_y.drop("Unnamed: 0", axis=1)
 
-    # foldData("Decision Tree", data_x_clean, data_y_clean)
-    #foldData("Neural Network", data_x_clean, data_y_clean)
-    #foldData("LDA", data_x_clean, data_y_clean)
-    #foldData("Support Vector Machine",data_x_clean, data_y_clean)
-    #foldData("NB", data_x_clean, data_y_clean)
+    foldData("Decision Tree", data_x_clean, data_y_clean)
+    foldData("Neural Network", data_x_clean, data_y_clean)
+    foldData("LDA", data_x_clean, data_y_clean)
+    foldData("Support Vector Machine",data_x_clean, data_y_clean)
+    foldData("NB", data_x_clean, data_y_clean)
     foldData("Nearest Neighbor", data_x_clean, data_y_clean)
 
 
 
 #Use preprocessing.py first to create processedData.csv
-#ofsted()
-timeFunction("NB")
+main()
+
 
