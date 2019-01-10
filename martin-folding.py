533b34e7c9bb3f3a4c50fd318059fea2d1289b8f
Pandelis Zembashis <info@pandelis.me>
2019-01-10 20:00:16 +0000

    Data and folding files

---
+++ martin-folding.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 15:57:10 2018
+
+@author: s15106137
+"""
+VERBOSE = False
+import warnings
+warnings.filterwarnings('ignore')
+
+def printScore(score_array, label,modelName):
+    import numpy as np
+    avg = np.mean(score_array)
+    std = np.std(score_array)
+    print("{:s}. {:s}. Mean: {:f} - Standard Deviation: {:f}".format(modelName,label,avg,std))
+
+#Evaluates model and gives f1-score. Generic to all algorithms
+def scoreModel(model, X_test, y_test):
+    from sklearn.metrics import f1_score
+    from sklearn.metrics import precision_score
+    from sklearn.metrics import recall_score
+    y_pred = model.predict(X_test)
+    from sklearn.metrics import classification_report
+    if(VERBOSE):
+        print(classification_report(y_test,y_pred))
+    return [f1_score(y_test, y_pred,average='weighted'), precision_score(y_test, y_pred,average='weighted'), recall_score(y_test, y_pred,average='weighted')]
+
+def foldData(modelName, X,y):
+    from sklearn.model_selection import StratifiedKFold
+    kfold = StratifiedKFold(10, True, 1)
+    f1_score_array = []
+    precision_score_array = []
+    recall_score_array = []
+    for train, test in kfold.split(X, y):
+        model = trainModel(modelName, X.iloc[train], y.iloc[train])
+        scores = scoreModel(model, X.iloc[test], y.iloc[test])
+        f1_score_array.append(scores[0])
+        precision_score_array.append(scores[1])
+        recall_score_array.append(scores[2])
+    printScore(f1_score_array, "F1 Score",modelName)
+    printScore(precision_score_array, "Precision Score",modelName)
+    printScore(recall_score_array, "Recall Score",modelName)
+
+#Method for training a specific model. Returns the model MODIFY FOR EACH ALGORITHM
+def trainModel(modelName, X_train, y_train):
+    if(modelName == "Decision Tree"):
+        from sklearn.tree import DecisionTreeClassifier
+        model = DecisionTreeClassifier(criterion='entropy', max_depth=10, random_state=0)
+        return model.fit(X_train, y_train)
+    if(modelName == "Neural Network"):
+        from sklearn.neural_network import MLPClassifier
+        mlp = MLPClassifier(hidden_layer_sizes=(24,24,24))
+        return mlp.fit(X_train,y_train)
+    if(modelName == "LDA"):
+        from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+        model = LinearDiscriminantAnalysis(n_components=2)
+        return model.fit(X_train, y_train)
+    if(modelName == "Support Vector Machine"):
+        from sklearn import svm
+        clf = svm.SVC(C=0.5, kernel='rbf', decision_function_shape='ovr')
+        return clf.fit(X_train, y_train)
+    if(modelName == "NB"):
+        from sklearn.naive_bayes import GaussianNB
+        model = GaussianNB()
+        return model.fit(X_train, y_train)
+
+def timeFunction(modelName):
+    import time
+    time1 = time.time()
+    ofsted()
+    time2 = time.time()
+    runtime = (time2 - time1) / 60
+    print(modelName, "algorithm " "took" , runtime, "minutes")
+
+#Main method tying together the whole flow and defines file/column names
+def ofsted():
+    import pandas as pd
+
+    X = pd.read_csv('data/x.csv')
+    X = X.drop("Unnamed: 0",axis=1)
+    y = pd.read_csv('data/y.csv')
+    y = y.drop("Unnamed: 0",axis=1)
+    #foldData("Decision Tree", X, y)
+    #foldData("Neural Network", X, y)
+    #foldData("LDA", X, y)
+    foldData("Support Vector Machine", X, y)
+    #foldData("NB", X, y)
+
+
+
+#Use preprocessing.py first to create processedData.csv
+#ofsted()
+timeFunction("NB")
+
---
+++ preprocessing.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 15:34:58 2018
+
+@author: s15106137
+"""
+
+VERBOSE = True
+
+
+import pandas as pd
+grouped = pd.read_csv('csv/england_cfrgrouped.csv')
+ofsted = pd.read_csv('csv/england_ofsted-schools.csv')
+joined = ofsted.set_index('URN').join(grouped.set_index('URN'),how="inner")
+joined.to_csv('joined.csv')
+print(joined.columns)
+
+def normaliseColumn(data, columnName):
+    colMin = data[columnName].min()
+    colMax = data[columnName].max()
+    
+    def norm(item):
+        return (item - colMin)/ (colMax - colMin)
+    data[columnName] = data[columnName].apply(norm)
+    return data
+
+def normaliseAllColumns(data):
+    exluded = ["URN", "Overall effectiveness"]
+    for column in data:
+        if (column not in exluded):
+            data = normaliseColumn(data, column)
+    return data     
+def removeComma(x):
+    if (type(x) is str):
+        try:
+            return float(x.split()[0].replace(',', ''))
+        except:
+            return x
+    return x
+
+def removeCommas(dataframe):
+    for column in dataframe.columns:
+        dataframe[column] = dataframe[column].apply(removeComma)
+    return dataframe
+
+def removeValue(dataframe, value):
+    def remove(x):
+        if (x == value):
+            return 0
+        return x
+    
+    for column in dataframe.columns:
+        dataframe[column] = dataframe[column].apply(remove)
+    return dataframe
+
+def convertColumnToFloat(dataframe, columnname):
+    from sklearn.preprocessing import LabelEncoder
+    le = LabelEncoder()
+    le.fit(dataframe[columnname].astype(str))
+    dataframe[columnname] = le.transform(dataframe[columnname].astype(str))
+    return dataframe
+    
+joined = joined.drop("Unnamed: 33",axis=1)
+joined = joined.drop("School name (as shown on Performance tables)",axis=1)
+joined = joined.drop("Old school name (if different)",axis=1)
+joined = joined.drop("LA name",axis=1)
+joined = joined.drop("LA code",axis=1)
+joined = joined.drop("Estab code",axis=1)
+joined = joined.drop("School DfE number",axis=1)
+joined = joined.drop("Phase of education",axis=1)
+joined = joined.drop("Phase for median group",axis=1)
+joined = joined.drop("TOTAL INCOME (£ per pupil)",axis=1)
+joined = joined.drop("TOTAL EXPENDITURE (£ per pupil)",axis=1)
+
+dropped = joined
+print(dropped.columns)
+dropped = convertColumnToFloat(dropped, "Establishment type")
+dropped = convertColumnToFloat(dropped, "Region")
+dropped = convertColumnToFloat(dropped, "London / Non-London")
+dropped = convertColumnToFloat(dropped, "FSM band")
+dropped = removeCommas(dropped)
+dropped = removeValue(dropped, "SUPP")
+dropped = removeValue(dropped, "..")
+dropped = removeValue(dropped, "")
+dropped = dropped.dropna(0, 'any')
+
+dropped = normaliseAllColumns(dropped)#
+print(type(dropped))
+dropped.to_csv('processedData.csv')
+print("done")
+
+
+def smoteIt(X, y):
+    from imblearn.over_sampling import SMOTE
+    if(VERBOSE):
+        print("Before SMOTE: {0} values".format(len(y.index)))
+    sm = SMOTE(random_state=42)
+    X, y = sm.fit_resample(X, y)
+    if(VERBOSE):
+        print("After SMOTE: {0} values".format(len(y)))
+    return X, y
+
+
+def splitData(data, target_field):
+    y = data.loc[:,[target_field]]
+    X = data.drop(target_field, axis=1)
+    return X, y
+
+
+X, y = splitData(dropped, "Overall effectiveness")
+smoteX, smotey = smoteIt(X, y)
+Xframe = pd.DataFrame(smoteX)
+print(Xframe)
+yframe = pd.DataFrame(smotey)
+Xframe.to_csv('X.csv')
+yframe.to_csv('y.csv')
